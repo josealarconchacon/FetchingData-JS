8ec93b0daebd6590c09ad2775f5dcc8cfce4d643
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-02-06 15:17:21 -0500

    fetch data with js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 // fetch data from api
 async function fetchData() {
-  const proxyURL = "https://cors-anywhere.herokuapp.com/";
-  const url = "http://api.forismatic.com/api/1.0/";
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const url =
+    "http://api.forismatic.com/api/1.0/?method=fetchData&lang=en&format=json";
 
   try {
-    const response = await fetch(`${proxyURL} ${url}`);
+    const response = await fetch(proxyUrl + url);
     const data = await response.json();
     console.log(data);
   } catch (error) {
-    console.log("No data available....");
+    fetchData();
+    console.log("No data available....", error);
   }
 }
+
+// load data
+fetchData();
